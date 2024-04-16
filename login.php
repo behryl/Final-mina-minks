0ab12b5bc61592a8fb513466cce7239aa939e254
behryl <132091984+behryl@users.noreply.github.com>
2024-04-16 21:36:26 +0000

    g

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-//error_reporting(0);
 include('includes/dbconnection.php');
-//error_reporting(0);
 
 if(isset($_POST['login']))
   {

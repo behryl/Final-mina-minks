4fdfc498204ee68489c65eea5169b755b87555cf
behryl <132091984+behryl@users.noreply.github.com>
2024-04-16 21:19:05 +0000

    h

--- actions/login_user_action.php
+++ actions/login_user_action.php
@@ -25,8 +25,7 @@ ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-//error_reporting(0);
+ //error_reporting(0);
 include('../includes/dbconnection.php');
 //error_reporting(0);
 

e158a3ffa017d974e2c0cd899cf2e8e15a28dcea
behryl <132091984+behryl@users.noreply.github.com>
2024-04-18 08:26:07 +0000

    j

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "bpmsdb");
+$con=mysqli_connect("127.0.0.1", "root", "+yy@H:7qGmB6", "bpmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
